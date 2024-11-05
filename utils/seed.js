60c6085110f87df2c41a4d161015525693955843
Fin M <qbres333@hotmail.com>
2024-11-04 20:42:45 -0500

    add seed.js code

--- utils/seed.js
+++ utils/seed.js
@@ -12,6 +12,35 @@ connection.on('error', (err) => err);
 connection.once('open', async () => {
     console.log('Connected');
 
-    // delete the collections if they exist
-    
-})
+    // delete the user/thought collections if they exist
+    let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
+    if(userCheck.length) {
+        await connection.dropCollection('users');
+    };
+
+    let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
+    if(thoughtCheck.length) {
+        await connection.dropCollection('thoughts');
+    };
+
+    // create an empty array to store user data
+    const users = [];
+
+    // loop 5 times to randomly assign data to users
+    for (let i = 0; i < 5; i++) {
+        const thoughts = getRandomThought(2); //randomly assign 2 thoughts
+        const reactions = getRandomReaction(2); //randomly assign 2 reactions
+        const username = getRandomUser();
+        const email = getRandomEmail();
+        // push the values into the array
+        users.push({ username, email, thoughts, reactions });
+    };
+
+    // add users to the collection
+    const userData = await User.create(users);
+    // log the data to show the data seeded to the database
+    console.table(users);
+    console.info('Seeding complete!');
+    process.exit(0);
+
+});
